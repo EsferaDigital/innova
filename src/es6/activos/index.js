486df9fedd9880095365af2577049df2ad911399
Gabriel Zavando <gabrielzavando@gmail.com>
2018-04-27 09:58:51 -0300

    termine el slider

--- src/es6/activos/index.js
+++ src/es6/activos/index.js
@@ -1,17 +1,47 @@
 //Slider
-(function ($) {
-	'use strict';
 
-	function efectos() {
-		$('.flexslider').flexslider({
-			animation: 'slide',
-			/*direction : 'vertical',*/
-			slideShow: true,
-			controlNav: true
-		});
-	}
-	$(document).ready(efectos);
-})(jQuery);
+var slider = $('#slider')
+var next = $('#btn-next')
+var prev = $('#btn-prev')
+
+//Movemos ultima imagen al primer lugar
+$('#slider section:last').insertBefore('#slider section:first')
+
+slider.css('margin-left', '-'+100+'%')
+
+function moverD(){
+	slider.animate({
+		marginLeft: '-'+200+'%'
+	}, 700, function(){
+		$('#slider section:first').insertAfter('#slider section:last')
+		slider.css('margin-left', '-' + 100 + '%')
+	})
+}
+
+function moverI() {
+	slider.animate({
+		marginLeft: 0
+	}, 700, function () {
+		$('#slider section:last').insertBefore('#slider section:first')
+		slider.css('margin-left', '-' + 100 + '%')
+	})
+}
+
+next.on('click', function(){
+	moverD()
+})
+
+prev.on('click', function(){
+	moverI()
+})
+
+function autoplay(){
+	interval = setInterval(function(){
+		moverD()
+	}, 5000)
+}
+
+autoplay()
 
 //Fin Slider
 
