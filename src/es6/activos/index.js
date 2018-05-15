52c19776bba1dfb538c22971ad2e0f58e4aee4f5
Gabriel Zavando <gabrielzavando@gmail.com>
2018-05-15 19:45:25 -0400

    renombramos colaboradores por empresas y añadimos modal a boton postular

--- src/es6/activos/index.js
+++ src/es6/activos/index.js
@@ -53,7 +53,8 @@ function onePageScroll(e) {
 		coordSection = $(idLink).offset().top
 
 	$('html, body').animate({
-		scrollTop: coordSection
+		//para separarlo un poco del top le puse (coordSection - 60)
+		scrollTop: (coordSection - 60)
 	}, 1000)
 }
 
@@ -172,7 +173,9 @@ const createCustomElement = (element, attributes, children) => {
 const modal1 = document.getElementById('modal1'),
 	modal2 = document.getElementById('modal2'),
 	modal3 = document.getElementById('modal3'),
-	modal4 = document.getElementById('modal4')
+	modal4 = document.getElementById('modal4'),
+	modalOculto = document.getElementById('oculto'),
+	modalPostula = document.getElementsByClassName('postula')
 
 //content es el parametro que recibe la funcion. En el meteremos el contenido del modal en si que va dentro del div modalContentEl
 const printModal = content => {
@@ -202,9 +205,11 @@ const printModal = content => {
 
 //Contenido de cada modal
 const contModal1 = `<h2 class="c-modal-item-title">Modal 1 Funciona</h2>`,
-	contModal2 = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSchS7ijK4Kh_jN3BZliG6wUS39kRFNyqgolHuZPBzVU1f-pJQ/viewform?embedded=true" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`,
+	contModal2 = `<h2 class="c-modal-item-title">Modal 2 Funciona</h2>`,
+	contPostula = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSchS7ijK4Kh_jN3BZliG6wUS39kRFNyqgolHuZPBzVU1f-pJQ/viewform?embedded=true" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`,
 	contModal3 = `<h2>Modal 3 Funciona</h2>`,
-	contModal4 = `<h2>Modal 4 Funciona</h2>`
+	contModal4 = `<h2>Modal 4 Funciona</h2>`,
+	contModalOculto = `<h2>Modal Oculto</h2>`
 
 //Ejecución de cada modal (esto se puede mejorar con un array de elementos)
 
@@ -227,3 +232,14 @@ modal3.addEventListener('click', () => {
 modal4.addEventListener('click', () => {
 	printModal(`${contModal4}`)
 })
+
+modalOculto.addEventListener('click', () => {
+	printModal(`${contModalOculto}`)
+})
+
+for (let n = 0; n < modalPostula.length; n++) {
+	modalPostula[n].addEventListener('click', (e) => {
+		e.preventDefault()
+		printModal(`${contPostula}`)
+	})
+}
